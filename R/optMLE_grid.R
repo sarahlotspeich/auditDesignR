cbda1cb3ab8547792f51e39deebd8f8ba5e15e39
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-08 17:19:03 -0500

    Allow window to widen if min not found

--- R/optMLE_grid.R
+++ R/optMLE_grid.R
@@ -1,20 +1,3 @@
-#' Adaptive grid search optimization to find the optMLE design
-#' @name optMLE_grid
-#' @param phI Phase I sample size.
-#' @param phII Phase II sample size.
-#' @param phI_strat Phase I stratum sample sizes, named list: \code{N00, N01, N10, N11}.
-#' @param min_n Minimum stratum size to be sampled.
-#' @param window_mult Window multiplier for how wide the grid should look on either side of the previous iteration's optimal design. The multiplier applies to the \code{audit_steps}; for example \code{window_mult = 1} (the DEFAULT) will look at a window of 1 times each step around the previous grid's optimal design.
-#' @param audit_steps A numeric vector (on the person scale). An audit step of 40 defines a grid with 40-person increments between designs. The elements of \code{audit_steps} should share common multiplies to avoid empty grid returns.
-#' @param Y_unval Column with the unvalidated outcome (can be name or numeric index).
-#' @param Y_val Column with the validated outcome (can be name or numeric index).
-#' @param X_unval Column(s) with the unvalidated predictors (can be name or numeric index).
-#' @param X_val Column(s) with the validated predictors (can be name or numeric index).
-#' @param indiv_score Matrix of score vectors for all parameters, \code{beta} and \code{eta}.
-#' @param errors_in Measurement error setting, options are \code{"Both"}, \code{"Y only"}, \code{"X only"}. Default is \code{"Both"}.
-#' @param return_full_grid If \code{TRUE}, all audits from all iterations of the grid search will be return. Default is \code{FALSE}.
-#' @return Scalar function value.
-#' @export
 optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_steps = c(40, 20, 10, 5, 1),
                         Y_unval, Y_val, X_unval, X_val, indiv_score, errors_in, return_full_grid = FALSE) {
 
@@ -121,14 +104,76 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_step
       ## And that the new minimum variance is <= the previous
       min_var <- min(new_grid$Vbeta)
       findOptimal <- sum(new_grid$Vbeta == min_var) == 1
-      if (findOptimal & round(min_var, 7) <= round(all_opt_des$Vbeta[step - 1], 7)) {
+
+      if (findOptimal & min_var <= all_opt_des$Vbeta[step - 1]) {
         min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
         all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
         if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
         if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
         prev_min <- min_var_design
         skippedLast <- FALSE
-      } else { skippedLast <- TRUE }
+      } else {
+        # Widen the window
+        # If skipped the last grid, keep the wider window
+        if (skippedLast & step > 2) {
+          # Run new grid
+          new_grid <- prop_grid(prop_min = pmax(0, (prev_grid_allocated - audit_windows[step - 2]) / n_to_allocate),
+                                prop_max = pmin(1, (prev_grid_allocated + audit_windows[step - 2]) / n_to_allocate),
+                                prop_inc = audit_steps_prop[step],
+                                phII = phII,
+                                phI_strat = phI_strat,
+                                min_n = min_n,
+                                n_to_allocate = n_to_allocate)
+        } else {
+          # Run new grid
+          new_grid <- prop_grid(prop_min = pmax(0, (prev_grid_allocated - audit_windows[step - 1]) / n_to_allocate),
+                                prop_max = pmin(1, (prev_grid_allocated + audit_windows[step - 1]) / n_to_allocate),
+                                prop_inc = audit_steps_prop[step],
+                                phII = phII,
+                                phI_strat = phI_strat,
+                                min_n = min_n,
+                                n_to_allocate = n_to_allocate)
+        }
+
+        if (nrow(new_grid) > 0) {
+          new_grid_list <- split(new_grid, seq(nrow(new_grid)))
+          for (r in 1:length(new_grid_list)) {
+            new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][,c("pi00", "pi01", "pi10", "pi11")]))
+          }
+
+          new_grid$Vbeta <- sapply(X = new_grid_list,
+                                   FUN = var_formula,
+                                   Y_unval = Y_unval,
+                                   Y_val = Y_val,
+                                   X_unval = X_unval,
+                                   X_val = X_val,
+                                   phI = phI,
+                                   indiv_score = indiv_score,
+                                   errors_in = errors_in)
+
+          # Check that a clear minimum was found
+          ## And that the new minimum variance is <= the previous
+          min_var <- min(new_grid$Vbeta)
+          findOptimal <- sum(new_grid$Vbeta == min_var) == 1
+          if (min_var <= all_opt_des$Vbeta[step - 1]) {
+            min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
+            all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
+            if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
+            if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
+            prev_min <- min_var_design
+            skippedLast <- FALSE
+          } else { skippedLast <- FALSE }
+        }
+      }
+      #
+      # if (findOptimal & round(min_var * 100, 6) <= round(all_opt_des$Vbeta[step - 1] * 100, 6)) {
+      #   min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
+      #   all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
+      #   if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
+      #   if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
+      #   prev_min <- min_var_design
+      #   skippedLast <- FALSE
+      # } else { skippedLast <- TRUE }
     } else { skippedLast <- TRUE }
     if (skippedLast) { all_opt_des[step, "Vbeta"] <- all_opt_des[step - 1, "Vbeta"]}
   }
