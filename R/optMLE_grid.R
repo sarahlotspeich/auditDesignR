a989674be308a028bf021a4c35b31ee4d6b2bafe
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-09 10:58:34 -0500

    Remove window widening, add grid_size to all_opt return

--- R/optMLE_grid.R
+++ R/optMLE_grid.R
@@ -1,4 +1,4 @@
-#' Adaptive grid search optimization to find the optMLE design
+#' Adaptive grid search optimization to find the optimal design (optMLE)
 #' @name optMLE_grid
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
@@ -15,7 +15,7 @@
 #' @param return_full_grid If \code{TRUE}, all audits from all iterations of the grid search will be return. Default is \code{FALSE}.
 #' @return Scalar function value.
 #' @export
-optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_steps = c(40, 20, 10, 5, 1),
+optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_steps = c(10, 1),
                         Y_unval, Y_val, X_unval, X_val, indiv_score, errors_in, return_full_grid = FALSE) {
 
   audit_windows <- window_mult * c(NA, audit_steps[-length(audit_steps)])
@@ -33,7 +33,7 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_step
                             prop_inc = audit_steps_prop,
                             n00 = NA, n01 = NA, n10 = NA, n11 = NA,
                             pi00 = NA, pi01 = NA, pi10 = NA, pi11 = NA,
-                            Vbeta = NA)
+                            Vbeta = NA, grid_size = NA)
 
   # Run initial grid search
   new_grid <- prop_grid(prop_min = c(0, 0, 0, 0),
@@ -67,6 +67,7 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_step
     new_grid <- new_grid[order(new_grid$Vbeta),]
     prev_min <- new_grid[1, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")]
     all_opt_des[1, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <-  prev_min
+    all_opt_des[1, "grid_size"] <- nrow(new_grid)
   } else {
     warning("Unable to find clear minimum. Please select a new starting value.")
     return(all_opt_des)
@@ -125,72 +126,12 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_step
       if (findOptimal & min_var <= all_opt_des$Vbeta[step - 1]) {
         min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
         all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
+        all_opt_des[step, "grid_size"] <- nrow(new_grid)
         if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
         if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
         prev_min <- min_var_design
         skippedLast <- FALSE
-      } else {
-        # Widen the window
-        # If skipped the last grid, keep the wider window
-        if (skippedLast & step > 2) {
-          # Run new grid
-          new_grid <- prop_grid(prop_min = pmax(0, (prev_grid_allocated - audit_windows[step - 2]) / n_to_allocate),
-                                prop_max = pmin(1, (prev_grid_allocated + audit_windows[step - 2]) / n_to_allocate),
-                                prop_inc = audit_steps_prop[step],
-                                phII = phII,
-                                phI_strat = phI_strat,
-                                min_n = min_n,
-                                n_to_allocate = n_to_allocate)
-        } else {
-          # Run new grid
-          new_grid <- prop_grid(prop_min = pmax(0, (prev_grid_allocated - audit_windows[step - 1]) / n_to_allocate),
-                                prop_max = pmin(1, (prev_grid_allocated + audit_windows[step - 1]) / n_to_allocate),
-                                prop_inc = audit_steps_prop[step],
-                                phII = phII,
-                                phI_strat = phI_strat,
-                                min_n = min_n,
-                                n_to_allocate = n_to_allocate)
-        }
-
-        if (nrow(new_grid) > 0) {
-          new_grid_list <- split(new_grid, seq(nrow(new_grid)))
-          for (r in 1:length(new_grid_list)) {
-            new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][,c("pi00", "pi01", "pi10", "pi11")]))
-          }
-
-          new_grid$Vbeta <- sapply(X = new_grid_list,
-                                   FUN = var_formula,
-                                   Y_unval = Y_unval,
-                                   Y_val = Y_val,
-                                   X_unval = X_unval,
-                                   X_val = X_val,
-                                   phI = phI,
-                                   indiv_score = indiv_score,
-                                   errors_in = errors_in)
-
-          # Check that a clear minimum was found
-          ## And that the new minimum variance is <= the previous
-          min_var <- min(new_grid$Vbeta)
-          findOptimal <- sum(new_grid$Vbeta == min_var) == 1
-          if (min_var <= all_opt_des$Vbeta[step - 1]) {
-            min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
-            all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
-            if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
-            if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
-            prev_min <- min_var_design
-            skippedLast <- FALSE
-          } else { skippedLast <- FALSE }
-        }
       }
-      #
-      # if (findOptimal & round(min_var * 100, 6) <= round(all_opt_des$Vbeta[step - 1] * 100, 6)) {
-      #   min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
-      #   all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
-      #   if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
-      #   if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
-      #   prev_min <- min_var_design
-      #   skippedLast <- FALSE
-      # } else { skippedLast <- TRUE }
     } else { skippedLast <- TRUE }
     if (skippedLast) { all_opt_des[step, "Vbeta"] <- all_opt_des[step - 1, "Vbeta"]}
   }
