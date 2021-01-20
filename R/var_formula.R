ca7289fa02b83db33bac9ef5f77366adecabf43b
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-20 15:21:03 -0600

    Delete commented out old code

--- R/var_formula.R
+++ R/var_formula.R
@@ -26,20 +26,9 @@ var_formula <- function(pi_vec = NULL, phI, indiv_score, sample_on) {
   } else {
     return(warning("You are attempting to sample on too many variables."))
   }
-  #
-  # if (errors_in == "Both") {
-  #   phI_vars <- c(Y_unval, X_unval)
-  # } else if (errors_in == "X only") {
-  #   phI_vars <- c(Y_val, X_unval)
-  # } else if (errors_in == "Y only") {
-  #   phI_vars <- c(Y_unval, X_val)
-  # }
-  # colnames(pi_probs)[1:2] <- phI_vars
 
   i <- info(indiv_score = indiv_score, pi_probs = pi_probs, sample_on = sample_on)
   cov <- tryCatch(expr = solve(i[1, 1] - i[1, -1] %*% solve(i[-1, -1]) %*% i[-1, 1]), error = function(err) {matrix(NA, nrow = nrow(i), ncol = ncol(i))})
-  #cov <- tryCatch(expr = solve(i), error = function(err) {matrix(NA, nrow = nrow(i), ncol = ncol(i))})
-  v_beta <- diag(cov)[1] / phI # round(diag(cov)[1] / phI, 8)
-  #se_beta <- sqrt(v_beta)
+  v_beta <- diag(cov)[1] / phI
   return(v_beta)
 }
