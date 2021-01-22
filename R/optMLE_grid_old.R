2224d8762fc7c3647a6a9d9f98c271c8579e2efe
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-22 11:57:10 -0600

    Replace optMLE_grid_old() with even older

--- R/optMLE_grid_old.R
+++ R/optMLE_grid_old.R
@@ -1,5 +1,5 @@
 #' Adaptive grid search optimization to find the optimal design (optMLE)
-#' @name optMLE_grid
+#' @name optMLE_grid_old
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
 #' @param phI_strat Phase I stratum sample sizes, named list: \code{N00, N01, N10, N11}.
@@ -7,50 +7,38 @@
 #' @param window_mult Window multiplier for how wide the grid should look on either side of the previous iteration's optimal design. The multiplier applies to the \code{audit_steps}; for example \code{window_mult = 1} (the DEFAULT) will look at a window of 1 times each step around the previous grid's optimal design.
 #' @param audit_steps A numeric vector (on the person scale). An audit step of 40 defines a grid with 40-person increments between designs. The elements of \code{audit_steps} should share common multiplies to avoid empty grid returns.
 #' @param max_window_mult If \code{window_mult} fails to find a minimum, it will increase up to \code{max_window_mult} and try again. The DEFAULT is \code{max_window_mult = 3}.
-#' @param sample_on Columns with the Phase I variables (should be categorical) used for sampling strata (can be name or numeric index). Currently, sampling on up to 3 variables can be accommodated.
+#' @param Y_unval Column with the unvalidated outcome (can be name or numeric index).
+#' @param Y_val Column with the validated outcome (can be name or numeric index).
+#' @param X_unval Column(s) with the unvalidated predictors (can be name or numeric index).
+#' @param X_val Column(s) with the validated predictors (can be name or numeric index).
 #' @param indiv_score Matrix of score vectors for all parameters, \code{beta} and \code{eta}.
+#' @param errors_in Measurement error setting, options are \code{"Both"}, \code{"Y only"}, \code{"X only"}. Default is \code{"Both"}.
 #' @param return_full_grid If \code{TRUE}, all audits from all iterations of the grid search will be return. Default is \code{FALSE}.
 #' @return Scalar function value.
 #' @export
 optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_steps, max_window_mult = 3,
-                        sample_on, indiv_score, return_full_grid = FALSE) {
+                        Y_unval, Y_val, X_unval, X_val, indiv_score, errors_in, return_full_grid = FALSE) {
 
   audit_windows <- window_mult * c(NA, audit_steps[-length(audit_steps)])
-  num_strat <- 2 ^ length(sample_on)
-  n_to_allocate <- phII - num_strat * min_n
+
+  # Since each of the 4 strata must have >= min_n subjects
+  ## The number that can be optimally allocated between them is only phII - 4 x min_n
+  n_to_allocate <- phII - 4 * min_n
+
+  # Calculate all_prop_inc based on step sizes all_n_inc and n_to_allocate
   audit_steps_prop <- audit_steps / n_to_allocate
-  if (length(sample_on) == 1) {
-    # Create a data frame to save the optimal designs from each grid search
-    all_opt_des <- data.frame(grid = 1:length(audit_steps),
-                              audit_steps, audit_windows,
-                              prop_inc = audit_steps_prop,
-                              n0 = NA, n1 = NA, pi0 = NA, pi1 = NA,
-                              Vbeta = NA, grid_size = NA)
-  } else if (length(sample_on) == 2) {
-    # Create a data frame to save the optimal designs from each grid search
-    all_opt_des <- data.frame(grid = 1:length(audit_steps),
-                              audit_steps, audit_windows,
-                              prop_inc = audit_steps_prop,
-                              n00 = NA, n01 = NA, n10 = NA, n11 = NA,
-                              pi00 = NA, pi01 = NA, pi10 = NA, pi11 = NA,
-                              Vbeta = NA, grid_size = NA)
-  } else if (length(sample_on) == 3) {
-    # Create a data frame to save the optimal designs from each grid search
-    all_opt_des <- data.frame(grid = 1:length(audit_steps),
-                              audit_steps, audit_windows,
-                              prop_inc = audit_steps_prop,
-                              n00_0 = NA, n01_0 = NA, n10_0 = NA, n11_0 = NA,
-                              n00_1 = NA, n01_1 = NA, n10_1 = NA, n11_1 = NA,
-                              pi00_0 = NA, pi01_0 = NA, pi10_0 = NA, pi11_0 = NA,
-                              pi00_1 = NA, pi01_1 = NA, pi10_1 = NA, pi11_1 = NA,
-                              Vbeta = NA, grid_size = NA)
-  } else {
-    return(warning("You are attempting to sample on too many variables."))
-  }
+
+  # Create a data frame to save the optimal designs from each grid search
+  all_opt_des <- data.frame(grid = 1:length(audit_steps),
+                            n_inc = audit_steps, audit_windows,
+                            prop_inc = audit_steps_prop,
+                            n00 = NA, n01 = NA, n10 = NA, n11 = NA,
+                            pi00 = NA, pi01 = NA, pi10 = NA, pi11 = NA,
+                            Vbeta = NA, grid_size = NA)
 
   # Run initial grid search
-  new_grid <- prop_grid(prop_min = rep(0, num_strat),
-                        prop_max = rep(1, num_strat),
+  new_grid <- prop_grid(prop_min = c(0, 0, 0, 0),
+                        prop_max = c(1, 1, 1, 1),
                         prop_inc = audit_steps_prop[1],
                         phI_strat = phI_strat,
                         phII = phII,
@@ -59,14 +47,18 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
 
   new_grid_list <- split(new_grid, seq(nrow(new_grid)))
   for (r in 1:length(new_grid_list)) {
-    new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][, grep(pattern = "pi", colnames(new_grid), value = TRUE)]))
+    new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][,c("pi00", "pi01", "pi10", "pi11")]))
   }
 
   new_grid$Vbeta <- sapply(X = new_grid_list,
                            FUN = var_formula,
+                           Y_unval = Y_unval,
+                           Y_val = Y_val,
+                           X_unval = X_unval,
+                           X_val = X_val,
                            phI = phI,
                            indiv_score = indiv_score,
-                           sample_on = sample_on)
+                           errors_in = errors_in)
 
   min_var <- min(new_grid$Vbeta)
 
@@ -82,8 +74,12 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
   # Check for a clear minimum
   findOptimal <- sum(new_grid$Vbeta == min_var) == 1
   if (findOptimal) {
-    min_var_design <- prev_min <- new_grid[new_grid$Vbeta == min_var,]
-    all_opt_des[1, -c(1:4)] <-  c(min_var_design, nrow(new_grid))
+    min_var_design <- new_grid[new_grid$Vbeta == min_var,]
+    # Sort grid by V(beta) smallest -> largest
+    new_grid <- new_grid[order(new_grid$Vbeta),]
+    prev_min <- new_grid[1, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")]
+    all_opt_des[1, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <-  prev_min
+    all_opt_des[1, "grid_size"] <- nrow(new_grid)
   } else {
     warning("Unable to find clear minimum. Please select a new starting value.")
     return(list("all_opt" = NA,
@@ -96,19 +92,18 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
 
   if (return_full_grid) { all_grids <- cbind(grid = 1, new_grid) }
 
-  #findOptimal <- findFinalOptimal <- FALSE
+  findOptimal <- findFinalOptimal <- FALSE
   for (step in 2:length(audit_steps_prop)) {
     # Reset audit window for this step
     step_mult <- window_mult
 
     # To choose min/max for next grid, use previous grid's "optimal" design
-    prev_grid_allocated <- prev_min[, 1:num_strat] - min_n
+    prev_grid_allocated <- prev_min[, c("n00", "n01", "n10", "n11")] - min_n
 
     # Run new grid
     new_grid <- prop_grid(prop_min = pmax(0, (prev_grid_allocated - audit_windows[step]) / n_to_allocate),
                           prop_max = pmin(1, (prev_grid_allocated + audit_windows[step]) / n_to_allocate),
                           prop_inc = audit_steps_prop[step],
-                          prev_grid = prev_grid_allocated,
                           phII = phII,
                           phI_strat = phI_strat,
                           min_n = min_n,
@@ -130,14 +125,18 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
     if (nrow(new_grid) > 0) {
       new_grid_list <- split(new_grid, seq(nrow(new_grid)))
       for (r in 1:length(new_grid_list)) {
-        new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][, grep(pattern = "pi", colnames(new_grid), value = TRUE)]))
+        new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][,c("pi00", "pi01", "pi10", "pi11")]))
       }
 
       new_grid$Vbeta <- sapply(X = new_grid_list,
                                FUN = var_formula,
+                               Y_unval = Y_unval,
+                               Y_val = Y_val,
+                               X_unval = X_unval,
+                               X_val = X_val,
                                phI = phI,
                                indiv_score = indiv_score,
-                               sample_on = sample_on)
+                               errors_in = errors_in)
 
       # Check that a clear minimum was found
       ## And that the new minimum variance is <= the previous
@@ -155,10 +154,12 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
       findOptimal <- sum(new_grid$Vbeta == min_var) == 1
 
       if (findOptimal & (min_var - all_opt_des$Vbeta[step - 1]) < 1E-8) {
-        min_var_design <- prev_min <- new_grid[new_grid$Vbeta == min_var,]
-        all_opt_des[step, -c(1:4)] <-  c(min_var_design, nrow(new_grid))
+        min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
+        all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
+        all_opt_des[step, "grid_size"] <- nrow(new_grid)
         if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
         if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
+        prev_min <- min_var_design
         try_wider <- FALSE
       } else { try_wider <- TRUE }
     }
@@ -178,14 +179,18 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
         if (nrow(new_grid) > 0) {
           new_grid_list <- split(new_grid, seq(nrow(new_grid)))
           for (r in 1:length(new_grid_list)) {
-            new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][, grep(pattern = "pi", colnames(new_grid), value = TRUE)]))
+            new_grid_list[[r]] <- as.numeric((new_grid_list[[r]][,c("pi00", "pi01", "pi10", "pi11")]))
           }
 
           new_grid$Vbeta <- sapply(X = new_grid_list,
                                    FUN = var_formula,
+                                   Y_unval = Y_unval,
+                                   Y_val = Y_val,
+                                   X_unval = X_unval,
+                                   X_val = X_val,
                                    phI = phI,
                                    indiv_score = indiv_score,
-                                   sample_on = sample_on)
+                                   errors_in = errors_in)
 
           # Check that a clear minimum was found
           ## And that the new minimum variance is <= the previous
@@ -203,10 +208,12 @@ optMLE_grid_old <- function(phI, phII, phI_strat, min_n, window_mult = 1, audit_
           findOptimal <- sum(new_grid$Vbeta == min_var) == 1
 
           if (findOptimal & (min_var - all_opt_des$Vbeta[step - 1]) < 1E-8) {
-            min_var_design <- prev_min <- new_grid[new_grid$Vbeta == min_var,]
-            all_opt_des[step, -c(1:4)] <-  c(min_var_design, nrow(new_grid))
+            min_var_design <- new_grid[new_grid$Vbeta == min_var, ]
+            all_opt_des[step, c("n00", "n01", "n10", "n11", "pi00", "pi01", "pi10", "pi11", "Vbeta")] <- min_var_design
+            all_opt_des[step, "grid_size"] <- nrow(new_grid)
             if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, new_grid)) }
             if (step == length(audit_steps_prop)) { findFinalOptimal <- TRUE }
+            prev_min <- min_var_design
           }
         }
       }
