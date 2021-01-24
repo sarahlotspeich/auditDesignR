90fec2150e5e511655d72c19bcef69c2ada5293d
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-24 09:14:14 -0600

    Finish and test grid_size()

--- R/grid_size.R
+++ R/grid_size.R
@@ -6,7 +6,7 @@
 #' @param sample_on Columns with the Phase I variables (should be categorical) used for sampling strata (can be name or numeric index). Currently, sampling on up to 3 variables can be accommodated.
 #' @param prev_grid_des If grid > 1, the audit from the previous iteration that was optimal.
 #' @return A vector of integers.
-grid_size <- function(delta, phi, num_strat, prev_grid_des = NULL) {
+grid_size <- function(delta, phi, num_strat, phI_strat, prev_grid_des = NULL, prev_delta = NULL) {
   # Stars and bars
   stars <- phi / delta
   bars <- num_strat - 1
@@ -15,9 +15,20 @@ grid_size <- function(delta, phi, num_strat, prev_grid_des = NULL) {
     return(choose(n = (stars + bars), k = (bars)))
   } else {
     # Number of all solutions (before taking prop_min/prop_max into account)
-    # all_sol <- choose(n = (stars + bars), k = (bars))
-    # Number of all solutions (before taking prop_min into account)
+    window_lb <- pmax(prev_grid_des - prev_delta, rep(0, num_strat)) / delta
+    window_ub <- pmin(prev_grid_des + prev_delta, unlist(phI_strat)) / delta
 
-    # Subtract off "bad" solutions, given the prop_min/prop_max window constraint
+    # Number of solutions >= window_lb
+    grid_size_lb <- choose(n = (stars + bars - sum(window_lb)), k = bars)
+    num_too_big <- 0
+
+    for (s in 1:length(window_ub)) {
+      too_big <- as.numeric(stars - (window_ub[s] + 1))
+      while (too_big >= 0 & choose(n = (too_big + (bars - 1) - sum(window_lb[-s])), k = (bars - 1)) > 0) {
+        num_too_big <- num_too_big + choose(n = (too_big + (bars - 1) - sum(window_lb[-s])), k = (bars - 1))
+        too_big <- too_big - 1
+      }
+    }
+    return(grid_size_lb - num_too_big)
   }
 }
