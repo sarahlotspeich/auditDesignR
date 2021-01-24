2f8a40b5205637904bc0abed6ae760a4de859952
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-24 17:05:19 -0600

    Fix error when return_full_grid = TRUE

--- R/optMLE_grid.R
+++ R/optMLE_grid.R
@@ -94,7 +94,7 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, sample_on, indiv_score, ret
       min_var_design <- prev_min <- grid[grid$Vbeta == min_var, ]
       all_opt_des <- rbind(all_opt_des,
                            cbind(grid = 1, audit_step = NA, min_var_design, grid_size = nrow(grid)))
-      if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = step, grid)) }
+      if (return_full_grid) { all_grids <- rbind(all_grids, cbind(grid = length(audit_steps), grid)) }
       if (audit_steps[length(audit_steps)] == 1) { findFinalOptimal <- TRUE }
     }
   }
