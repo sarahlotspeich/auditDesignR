57653ece6511142ba107387484a5fa685d2e5618
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-24 16:42:36 -0600

    Export var_by_row()

--- R/var_by_row.R
+++ R/var_by_row.R
@@ -1,3 +1,4 @@
+#' @export
 var_by_row <- function(row, phI, indiv_score, sample_on) {
   pi_vec <- row[(length(row)/2+1):length(row)]
   Vbeta <- var_formula(pi_vec = pi_vec, phI = phI, indiv_score = indiv_score, sample_on = sample_on)
