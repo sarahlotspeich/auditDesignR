8d90830cfa2749242bf6b4616926a1332eac3405
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-02-02 12:30:21 -0600

    sample_bcc() add sample_on parameter
    Allows sampling on 1, 2, or 3 binary variables

--- R/sample_bcc.R
+++ R/sample_bcc.R
@@ -1,75 +1,152 @@
 #' Sample unvalidated balanced case-control (BCC*) audit based on Phase I variables.
 #' @name sample_bcc
-#' @param dat Dataframe or matrix containing columns \code{Y_unval}, \code{Y_val}, \code{X_unval}, \code{X_val}.
-#' @param Y_unval Column with the unvalidated outcome (can be name or numeric index). (Left \code{NULL} if \code{errors_in = "X only"}.)
-#' @param Y_val Column with the validated outcome (can be name or numeric index).
-#' @param X_unval Column(s) with the unvalidated predictors (can be name or numeric index). (Left \code{NULL} if \code{errors_in = "Y only"}.)
-#' @param X_val Column(s) with the validated predictors (can be name or numeric index).
+#' @param dat Dataframe or matrix containing columns \code{sample_on}.
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
-#' @param errors_in Measurement error setting, options are \code{"Both"}, \code{"Y only"}, \code{"X only"}. Default is \code{"Both"}.
+#' @param sample_on Columns with the Phase I variables (should be categorical) used for sampling strata (can be name or numeric index). Currently, sampling on up to 3 binary variables can be accommodated.
 #' @return A vector of length \code{phI} with validation indicators V = 1 if selected for Phase II and V = 0 otherwise.
 #' @export
-sample_bcc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, phII, errors_in = "Both") {
+sample_bcc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, phII, sample_on) {
 
-  if (errors_in == "Both") {
-    # Cross-tabulate Phase I data (Y*,X*)
-    phI_tab <- table(dat[, Y_unval], dat[, X_unval])
+  # Cross-tabulate Phase I data (sample_on)
+  phI_tab <- table(dat[, sample_on])
+  num_strat <- 2 ^ length(sample_on)
 
-    # Create index vectors for subjects based on (Y*, X*)
-    ind_N11 <- which(dat[, Y_unval] == 1 & dat[, X_unval] == 1)
-    ind_N10 <- which(dat[, Y_unval] == 1 & dat[, X_unval] == 0)
-    ind_N01 <- which(dat[, Y_unval] == 0 & dat[, X_unval] == 1)
-    ind_N00 <- which(dat[, Y_unval] == 0 & dat[, X_unval] == 0)
-  } else if (errors_in == "Y only") {
-    # Cross-tabulate Phase I data (Y*,X)
-    phI_tab <- table(dat[, Y_unval], dat[, X_val])
+  if (num_strat == 2) {
+    # Create index vectors for subjects
+    ind_N1 <- which(dat[, sample_on[1]] == 1)
+    ind_N0 <- which(dat[, sample_on[1]] == 0)
 
-    # Create index vectors for subjects based on (Y*, X)
-    ind_N11 <- which(dat[, Y_unval] == 1 & dat[, X_val] == 1)
-    ind_N10 <- which(dat[, Y_unval] == 1 & dat[, X_val] == 0)
-    ind_N01 <- which(dat[, Y_unval] == 0 & dat[, X_val] == 1)
-    ind_N00 <- which(dat[, Y_unval] == 0 & dat[, X_val] == 0)
-  } else if (errors_in == "X only") {
-    # Cross-tabulate Phase I data (Y,X*)
-    phI_tab <- table(dat[, Y_val], dat[, X_unval])
+    # And stratum sizes for full cohort
+    N1 <- length(ind_N1)
+    N0 <- length(ind_N0)
 
-    # Create index vectors for subjects based on (Y, X*)
-    ind_N11 <- which(dat[, Y_val] == 1 & dat[, X_unval] == 1)
-    ind_N10 <- which(dat[, Y_val] == 1 & dat[, X_unval] == 0)
-    ind_N01 <- which(dat[, Y_val] == 0 & dat[, X_unval] == 1)
-    ind_N00 <- which(dat[, Y_val] == 0 & dat[, X_unval] == 0)
+    phI_strat <- list(N0 = N0, N1 = N1)
+  } else if (num_strat == 4) {
+    # Create index vectors for subjects
+    ind_N11 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 1)
+    ind_N10 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 0)
+    ind_N01 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 1)
+    ind_N00 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 0)
+
+    # And stratum sizes for full cohort
+    N11 <- length(ind_N11)
+    N10 <- length(ind_N10)
+    N01 <- length(ind_N01)
+    N00 <- length(ind_N00)
+
+    phI_strat <- list(N00 = N00, N01 = N01, N10 = N10, N11 = N11)
+  } else if (num_strat == 8) {
+    # Create index vectors for subjects
+    ind_N11_0 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 1 & dat[, sample_on[3]] == 0)
+    ind_N10_0 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 0 & dat[, sample_on[3]] == 0)
+    ind_N01_0 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 1 & dat[, sample_on[3]] == 0)
+    ind_N00_0 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 0 & dat[, sample_on[3]] == 0)
+    ind_N11_1 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 1 & dat[, sample_on[3]] == 1)
+    ind_N10_1 <- which(dat[, sample_on[1]] == 1 & dat[, sample_on[2]] == 0 & dat[, sample_on[3]] == 1)
+    ind_N01_1 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 1 & dat[, sample_on[3]] == 1)
+    ind_N00_1 <- which(dat[, sample_on[1]] == 0 & dat[, sample_on[2]] == 0 & dat[, sample_on[3]] == 1)
+
+    # And stratum sizes for full cohort
+    N11_0 <- length(ind_N11_0)
+    N10_0 <- length(ind_N10_0)
+    N01_0 <- length(ind_N01_0)
+    N00_0 <- length(ind_N00_0)
+    N11_1 <- length(ind_N11_1)
+    N10_1 <- length(ind_N10_1)
+    N01_1 <- length(ind_N01_1)
+    N00_1 <- length(ind_N00_1)
+
+    phI_strat <- list(N00_0 = N00_0, N01_0 = N01_0, N10_0 = N10_0, N11_0 = N11_0,
+                      N00_1 = N00_1, N01_1 = N01_1, N10_1 = N10_1, N11_1 = N11_1)
   }
 
-  N00 <- phI_tab[1,1]; N01 <- phI_tab[1,2]; N10 <- phI_tab[2,1]; N11 <- phI_tab[2,2]
-  phI_strat <- list(N00 = N00, N01 = N01, N10 = N10, N11 = N11)
+  # If data are available, take phII / num_strat from each of the strata
+  aud <- pmin(unlist(phI_strat), floor(phII / num_strat))
 
-  # If data are available, take n / 4 from each of the strata
-  aud <- pmin(c(phI_strat$N11, phI_strat$N10, phI_strat$N01, phI_strat$N00), rep(phII / 4, 4))
+  # If any strata were too small to take phII / num_strat, allocate remaining audit evenly between remaining strata
+  if (num_strat == 2) {
+    while (sum(aud) < phII) {
+      if (aud$N1 < phI_strat$N1) {
+        aud$N1 <- aud$N1 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N0 < phI_strat$N0) {
+        aud$N0 <- aud$N0 + 1
+        if (sum(aud) == phII) break()
+      }
+    }
 
-  # If any strata were too small to take n / 4, allocate remaining audit evenly between remaining strata
-  while (sum(aud) < phII) {
-    if (aud[1] < phI_strat$N11) {
-      aud[1] <- aud[1] + 1
-      if (sum(aud) == phII) break()
+    V_bcc <- as.numeric(seq(1, phI) %in% c(sample(x = ind_N0, size = aud$N0, replace = F),
+                                           sample(x = ind_N1, size = aud$N1, replace = F)))
+  } else if (num_strat == 4) {
+    while (sum(aud) < phII) {
+      if (aud$N11 < phI_strat$N11) {
+        aud$N11 <- aud$N11 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N10 < phI_strat$N10) {
+        aud$N10 <- aud$N10 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N01 < phI_strat$N01) {
+        aud$N01 <- aud$N01 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N00 < phI_strat$N00) {
+        aud$N00 <- aud$N00 + 1
+        if (sum(aud) == phII) break()
+      }
     }
-    if (aud[2] < phI_strat$N10) {
-      aud[2] <- aud[2] + 1
-      if (sum(aud) == phII) break()
+
+    V_bcc <- as.numeric(seq(1, phI) %in% c(sample(x = ind_N11, size = aud$N11, replace = F),
+                                           sample(x = ind_N10, size = aud$N10, replace = F),
+                                           sample(x = ind_N01, size = aud$N01, replace = F),
+                                           sample(x = ind_N00, size = aud$N00, replace = F)))
+  } else if (num_strat == 8) {
+    while (sum(aud) < phII) {
+      if (aud$N11_1 < phI_strat$N11_1) {
+        aud$N11_1 <- aud$N11_1 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N11_0 < phI_strat$N11_0) {
+        aud$N11_0 <- aud$N11_0 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N10_1 < phI_strat$N10_1) {
+        aud$N10_1 <- aud$N10_1 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N10_0 < phI_strat$N10_0) {
+        aud$N10_0 <- aud$N10_0 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N01_1 < phI_strat$N01_1) {
+        aud$N01_1 <- aud$N01_1 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N01_0 < phI_strat$N01_0) {
+        aud$N01_0 <- aud$N01_0 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N00_1 < phI_strat$N00_1) {
+        aud$N00_1 <- aud$N00_1 + 1
+        if (sum(aud) == phII) break()
+      }
+      if (aud$N00_0 < phI_strat$N00_0) {
+        aud$N00_0 <- aud$N00_0 + 1
+        if (sum(aud) == phII) break()
+      }
     }
-    if (aud[3] < phI_strat$N01) {
-      aud[3] <- aud[3] + 1
-      if (sum(aud) == phII) break()
-    }
-    if (aud[4] < phI_strat$N00) {
-      aud[4] <- aud[4] + 1
-      if (sum(aud) == phII) break()
-    }
+
+    V_bcc <- as.numeric(seq(1, phI) %in% c(sample(x = ind_N11_1, size = aud$N11_1, replace = F),
+                                           sample(x = ind_N10_1, size = aud$N10_1, replace = F),
+                                           sample(x = ind_N01_1, size = aud$N01_1, replace = F),
+                                           sample(x = ind_N00_1, size = aud$N00_1, replace = F),
+                                           sample(x = ind_N11_0, size = aud$N11_0, replace = F),
+                                           sample(x = ind_N10_0, size = aud$N10_0, replace = F),
+                                           sample(x = ind_N01_0, size = aud$N01_0, replace = F),
+                                           sample(x = ind_N00_0, size = aud$N00_0, replace = F)))
   }
-
-  V_bcc <- as.numeric(seq(1, phI) %in% c(sample(x = ind_N11, size = aud[1], replace = F),
-                                         sample(x = ind_N10, size = aud[2], replace = F),
-                                         sample(x = ind_N01, size = aud[3], replace = F),
-                                         sample(x = ind_N00, size = aud[4], replace = F)))
   return(V_bcc)
 }
