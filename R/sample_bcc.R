c63dd9662c46150b5c64afe125b1d931eaa59ef6
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-02-02 12:41:39 -0600

    Remove old params, debug sample_bcc()

--- R/sample_bcc.R
+++ R/sample_bcc.R
@@ -6,7 +6,7 @@
 #' @param sample_on Columns with the Phase I variables (should be categorical) used for sampling strata (can be name or numeric index). Currently, sampling on up to 3 binary variables can be accommodated.
 #' @return A vector of length \code{phI} with validation indicators V = 1 if selected for Phase II and V = 0 otherwise.
 #' @export
-sample_bcc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, phII, sample_on) {
+sample_bcc <- function(dat, phI, phII, sample_on) {
 
   # Cross-tabulate Phase I data (sample_on)
   phI_tab <- table(dat[, sample_on])
@@ -62,40 +62,40 @@ sample_bcc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, p
   }
 
   # If data are available, take phII / num_strat from each of the strata
-  aud <- pmin(unlist(phI_strat), floor(phII / num_strat))
+  aud <- as.list(pmin(unlist(phI_strat), floor(phII / num_strat)))
 
   # If any strata were too small to take phII / num_strat, allocate remaining audit evenly between remaining strata
   if (num_strat == 2) {
-    while (sum(aud) < phII) {
+    while (sum(unlist(aud)) < phII) {
       if (aud$N1 < phI_strat$N1) {
         aud$N1 <- aud$N1 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N0 < phI_strat$N0) {
         aud$N0 <- aud$N0 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
     }
 
     V_bcc <- as.numeric(seq(1, phI) %in% c(sample(x = ind_N0, size = aud$N0, replace = F),
                                            sample(x = ind_N1, size = aud$N1, replace = F)))
   } else if (num_strat == 4) {
-    while (sum(aud) < phII) {
+    while (sum(unlist(aud)) < phII) {
       if (aud$N11 < phI_strat$N11) {
         aud$N11 <- aud$N11 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N10 < phI_strat$N10) {
         aud$N10 <- aud$N10 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N01 < phI_strat$N01) {
         aud$N01 <- aud$N01 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N00 < phI_strat$N00) {
         aud$N00 <- aud$N00 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
     }
 
@@ -104,38 +104,38 @@ sample_bcc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, p
                                            sample(x = ind_N01, size = aud$N01, replace = F),
                                            sample(x = ind_N00, size = aud$N00, replace = F)))
   } else if (num_strat == 8) {
-    while (sum(aud) < phII) {
+    while (sum(unlist(aud)) < phII) {
       if (aud$N11_1 < phI_strat$N11_1) {
         aud$N11_1 <- aud$N11_1 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N11_0 < phI_strat$N11_0) {
         aud$N11_0 <- aud$N11_0 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N10_1 < phI_strat$N10_1) {
         aud$N10_1 <- aud$N10_1 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N10_0 < phI_strat$N10_0) {
         aud$N10_0 <- aud$N10_0 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N01_1 < phI_strat$N01_1) {
         aud$N01_1 <- aud$N01_1 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N01_0 < phI_strat$N01_0) {
         aud$N01_0 <- aud$N01_0 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N00_1 < phI_strat$N00_1) {
         aud$N00_1 <- aud$N00_1 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
       if (aud$N00_0 < phI_strat$N00_0) {
         aud$N00_0 <- aud$N00_0 + 1
-        if (sum(aud) == phII) break()
+        if (sum(unlist(aud)) == phII) break()
       }
     }
 
