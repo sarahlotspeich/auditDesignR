358ff2179c6e09aa31622d19efc5267fdf35120d
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-02-19 11:26:31 -0600

    Incorporate phIIa_strat parameter throughout optMLE_grid()

--- R/optMLE_grid.R
+++ R/optMLE_grid.R
@@ -2,7 +2,8 @@
 #' @name optMLE_grid
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
-#' @param phI_strat Phase I stratum sample sizes, named list.
+#' @param phI_strat Phase I stratum sample sizes as a named list, dataframe, or vector
+#' @param phIIa_strat For multi-wave designs, Phase II(a) stratum sample sizes as a named list, dataframe, or vector Default is \code{NULL}.
 #' @param min_n Minimum stratum size to be sampled.
 #' @param sample_on Columns with the Phase I variables (should be categorical) used for sampling strata (can be name or numeric index). Currently, sampling on up to 3 binary variables can be accommodated.
 #' @param indiv_score Matrix of score vectors for all parameters.
@@ -16,7 +17,7 @@
 #' \item{full_grid_search}{If \code{return_full_grid} = TRUE, a dataframe containing all grids from ann iterations.}
 #' \item{message}{Result of the grid search, options include \code{"No valid grids"}, \code{"Singular information"}, \code{"Tie for minimum"}, \code{"Grid completed without finding minimum"}, \code{"Grid search successful"}.}
 #' @export
-optMLE_grid <- function(phI, phII, phI_strat, min_n, sample_on, indiv_score, return_full_grid = FALSE, max_grid_size = 10000) {
+optMLE_grid <- function(phI, phII, phI_strat, phIIa_strat = NULL, min_n, sample_on, indiv_score, return_full_grid = FALSE, max_grid_size = 10000) {
   # Since each of the strata must have >= min_n subjects
   ## The number that can be optimally allocated between them is only phII - num_strat x min_n
   num_strat <- 2 ^ length(sample_on)
@@ -42,7 +43,7 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, sample_on, indiv_score, ret
   }
 
   # Run initial grid search
-  grid <- build_grid(delta = audit_steps, phi = phi, num_strat = num_strat, phI_strat = phI_strat, min_n = min_n, prev_grid_des = NULL, prev_delta = NULL)
+  grid <- build_grid(delta = audit_steps, phi = phi, num_strat = num_strat, phI_strat = phI_strat, phIIa_strat = phIIa_strat, min_n = min_n, prev_grid_des = NULL, prev_delta = NULL)
   grid$Vbeta <- apply(X = grid, MARGIN = 1, FUN = var_by_row, phI = phI, indiv_score = indiv_score, sample_on = sample_on)
   min_var <- min(grid$Vbeta)
 
@@ -99,7 +100,7 @@ optMLE_grid <- function(phI, phII, phI_strat, min_n, sample_on, indiv_score, ret
     }
 
     # Run initial grid search
-    grid <- build_grid(delta = audit_steps[length(audit_steps)], phi = phi, num_strat = num_strat, phI_strat = phI_strat, min_n = min_n, prev_grid_des = prev_grid_des, prev_delta = audit_steps[length(audit_steps) - 1])
+    grid <- build_grid(delta = audit_steps[length(audit_steps)], phi = phi, num_strat = num_strat, phI_strat = phI_strat, phIIa_strat = phIIa_strat, min_n = min_n, prev_grid_des = prev_grid_des, prev_delta = audit_steps[length(audit_steps) - 1])
     grid$Vbeta <- apply(X = grid, MARGIN = 1, FUN = var_by_row, phI = phI, indiv_score = indiv_score, sample_on = sample_on)
     min_var <- min(grid$Vbeta)
 
