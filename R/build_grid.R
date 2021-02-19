771cf789b2d061131f55b958754b988db903a20a
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-02-19 11:24:48 -0600

    Incorporate phIIa_strat param into build_grid()
    Allow grid to add in the number of subjects already sampled in Phase II(a) in a multi-wave design

--- R/build_grid.R
+++ R/build_grid.R
@@ -1,16 +1,17 @@
 #' Build audit grid
 #' @name grid_size
 #' @param delta Audit step size (in people).
-#' @param phi Number of people to be allocated: \code{phi = phII - num_strat * min_n}.
+#' @param phi Number of people to be allocated.
 #' @param num_strat Number of strata on which sampling is based. Currently handles \code{num_strat} = 2, 4, or 8.
-#' @param phI_strat Phase I stratum sample sizes, named list.
+#' @param phI_strat Phase I stratum sample sizes as a named list, dataframe, or vector
+#' @param phIIa_strat For multi-wave designs, Phase II(a) stratum sample sizes as a named list, dataframe, or vector. Default is \code{NULL}.
 #' @param closed For multi-wave designs, a vector of names for strata that are "closed", meaning we do not wish to sample from them. Default is \code{NULL}.
 #' @param closed_at For multi-wave designs, a vector of already sampled sizes for strata that are "closed" (must be the same length as \code{closed}). Default is \code{NULL}.
 #' @param min_n Minimum stratum size to be sampled.
 #' @param prev_grid_des If grid > 1, the audit from the previous iteration that was optimal.
 #' @param prev_delta If grid > 1, the step size from the previous iteration.
 #' @export
-build_grid <- function(delta, phi, num_strat, phI_strat, closed = NULL, closed_at = NULL, min_n, prev_grid_des = NULL, prev_delta = NULL) {
+build_grid <- function(delta, phi, num_strat, phI_strat, phIIa_strat = NULL, closed = NULL, closed_at = NULL, min_n, prev_grid_des = NULL, prev_delta = NULL) {
   stars <- phi / delta
 
   # Exclude closed strata
@@ -26,12 +27,6 @@ build_grid <- function(delta, phi, num_strat, phI_strat, closed = NULL, closed_a
     window_lb <- floor(pmax(prev_grid_des - prev_delta, rep(0, num_strat)) / delta)
     window_ub <- floor(pmin(prev_grid_des + prev_delta, (unlist(phI_strat) - pmin(unlist(phI_strat), min_n))) / delta)
   }
-
-  # If strata have been closed, force lb/ub to both be value from closed_at (already sampled)
-  # if (length(closed) > 0) {
-  #   window_lb[names(phI_strat) ==  toupper(closed)] <-
-  #     window_ub[names(phI_strat) ==  toupper(closed)] <- unlist(closed_at) / delta
-  # }
 
   grid <- do.call(expand.grid, grid_vals(x = window_lb, y = window_ub))
 
@@ -50,6 +45,13 @@ build_grid <- function(delta, phi, num_strat, phI_strat, closed = NULL, closed_a
   grid <- grid +
     matrix(data = pmin(unlist(phI_strat), min_n), nrow = nrow(grid), ncol = ncol(grid), byrow = TRUE)
   colnames(grid) <- gsub("N", "n", names(phI_strat))
+
+  # If multi-wave design, also add the stratum sample sizes from Phase II(a)
+  if (!is.null(phIIa_strat)) {
+    for (c in colnames(grid)) {
+      grid[, c] <- grid[, c] + as.numeric(phIIa_strat[c])
+    }
+  }
 
   # Augment with sampling probabilities
   pi_grid <- matrix(nrow = nrow(grid), ncol = ncol(grid))
