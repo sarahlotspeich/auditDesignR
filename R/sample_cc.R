dfad8bf235eba5cde790df2b0d837c0c3e2ad109
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-03-23 12:01:32 -0500

    Revise sample_cc() to take in a sample_on parameter and allow K-level categorical var

--- R/sample_cc.R
+++ R/sample_cc.R
@@ -1,50 +1,32 @@
 #' Sample unvalidated case-control (CC*) audit based on Phase I variables.
 #' @name sample_cc
-#' @param dat Dataframe or matrix containing columns \code{Y_unval}, \code{Y_val}, \code{X_unval}, \code{X_val}.
-#' @param Y_unval Column with the unvalidated outcome (can be name or numeric index). (Left \code{NULL} if \code{errors_in = "X only"}.)
-#' @param Y_val Column with the validated outcome (can be name or numeric index).
-#' @param X_unval Column(s) with the unvalidated predictors (can be name or numeric index). (Left \code{NULL} if \code{errors_in = "Y only"}.)
-#' @param X_val Column(s) with the validated predictors (can be name or numeric index).
+#' @param dat Dataframe or matrix containing columns \code{sample_on}.
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
-#' @param errors_in Measurement error setting, options are \code{"Both"}, \code{"Y only"}, \code{"X only"}. Default is \code{"Both"}.
+#' @param sample_on Column with the Phase I outcome variable (should be categorical) used for sampling strata (can be name or numeric index).
 #' @return A vector of length \code{phI} with validation indicators V = 1 if selected for Phase II and V = 0 otherwise.
 #' @export
-sample_cc <- function(dat, Y_unval = NULL, Y_val, X_unval = NULL, X_val, phI, phII, errors_in = "Both") {
-  if (errors_in == "Both" || errors_in == "Y only") {
-    # Cross-tabulate Phase I data (Y*)
-    phI_tab <- table(dat[, Y_unval])
+sample_cc <- function(dat, phI, phII, sample_on) {
+  # Cross-tabulate Phase I data (Y*)
+  phI_tab <- data.frame(table(dat[, sample_on]))
+  colnames(phI_tab)[1] <- sample_on
+  colnames(phI_tab)[2] <- "N"
+  phI_tab$target <- pmin(floor(phII / nrow(phI_tab)), phI_tab$N)
 
-    cases <- phI_tab[1]
-    controls <- phI_tab[2]
-
-    # Create index vectors for subjects based on (Y*)
-    ind_N0. <- which(dat[, Y_unval] == 0)
-    ind_N1. <- which(dat[, Y_unval] == 1)
-  } else if (errors_in == "X only") {
-    # Cross-tabulate Phase I data (Y*)
-    phI_tab <- table(dat[, Y_val])
-
-    cases <- phI_tab[1]
-    controls <- phI_tab[2]
-
-    # Create index vectors for subjects based on (Y)
-    ind_N0. <- which(dat[, Y_val] == 0)
-    ind_N1. <- which(dat[, Y_val] == 1)
+  # If any of the strata were too small, redistribute leftover
+  while(sum(phI_tab$target) < phII) {
+    for (i in 1:nrow(phI_tab)) {
+      if (phI_tab$N[i] > phI_tab$target[i] & sum(phI_tab$target) < phII) {
+        phI_tab$target[i] <- phI_tab$target[i] + 1
+      }
+    }
   }
 
-  target_cases <- min(phII / 2, cases)
-  target_controls <- min(phII / 2, controls)
-
-  if (target_cases < phII / 2) {
-    target_controls <- target_controls + (phII / 2 - target_cases)
-  } else if (target_controls < phII / 2) {
-    target_cases <- target_cases + (phII / 2 - target_controls)
+  V_cc <- vector()
+  for (i in 1:nrow(phI_tab)) {
+    V_cc <- append(V_cc, sample(x = which(dat[, sample_on] == phI_tab[i, sample_on]), size = phI_tab[i, "target"], replace = F))
   }
 
-  # Randomly sample target_controls/target_cases from ind_N0. and ind_N1., respectively
-  V_cc <- c(sample(x = ind_N1., size = target_cases, replace = F),
-            sample(x = ind_N0., size = target_controls, replace = F))
   V_cc <- as.numeric(seq(1, phI) %in% V_cc[order(V_cc)])
   return(V_cc)
 }
