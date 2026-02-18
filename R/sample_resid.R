299e04feab7613435d510526e10ac8d4167caae9
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-18 10:23:07 -0500

    Add "negbin" option to sample_resid()

--- R/sample_resid.R
+++ R/sample_resid.R
@@ -1,13 +1,15 @@
 #' Sample residual sample audit based on naive model from Phase I variables.
 #' @name sample_resid
 #' @param formula Model formula used to calculate residuals, passed to \code{glm()}.
-#' @param family Type of model to be used to fit \code{formula}, passed to \code{glm()}. Currently, the function accepts \code{family = "gaussian", "binomial",} or \code{"poisson"}.
+#' @param family Type of model to be used to fit \code{formula}, passed to \code{glm()} or \code{glm.nb()}. Currently, the function accepts \code{family = "gaussian", "binomial", "poisson",} or \code{"negbin"}.
 #' @param dat Dataframe or matrix containing variables from \code{formula}.
 #' @param phI Phase I sample size.
 #' @param phII Phase II sample size.
 #' @param wave1_Validated (For use with multi-wave designs) A logical vector with the validation indicator from first wave of audits.
 #' @return A vector of length \code{phI} with validation indicators V = 1 if selected for Phase II and V = 0 otherwise.
 #' @export
+#' @importFrom MASS glm.nb
+#' @importFrom stats glm
 sample_resid <- function(formula, family, dat, phI, phII, wave1_Validated = NULL) {
   ## If single wave, set wave1_validated = FALSE for all
   if (length(wave1_Validated) == 0) {
@@ -15,9 +17,14 @@ sample_resid <- function(formula, family, dat, phI, phII, wave1_Validated = NULL
   }
   
   ## Fit user-specified model
-  fit = glm(formula = as.formula(formula), 
-            family = family, 
-            data = dat)
+  if (family == "negbin") {
+    fit = glm.nb(formula = as.formula(formula), 
+                 data = dat)  
+  } else {
+    fit = glm(formula = as.formula(formula), 
+              family = family, 
+              data = dat)
+  }
   
   ## Get linear predictor
   mu = predict(fit)
@@ -29,7 +36,7 @@ sample_resid <- function(formula, family, dat, phI, phII, wave1_Validated = NULL
   } else if (family == "binomial") {
     ## Residuals for logistic regression: Y - 1 / (1 + e ^ mu)
     r = as.vector(fit$y - (1 + exp(- mu)) ^ (- 1))
-  } else if (family == "poisson") {
+  } else if (family %in% c("poisson", "negbin")) {
     ## Residuals for Poisson regression: Y - e ^ mu
     r = as.vector(fit$y - exp(mu))  
   }
